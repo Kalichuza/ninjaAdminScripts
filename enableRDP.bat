b2899e3e8193d15f66e902edadfa334ef2433a94
Kalichuza <kalichuza@proton.me>
2024-06-07 15:02:00 -0400

    Create enableRDP.bat
    Enables RDP with no reboot

---
+++ enableRDP.bat
@@ -0,0 +1,5 @@
+netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+net stop UmRdpService
+net stop TermService
+net start TermService
