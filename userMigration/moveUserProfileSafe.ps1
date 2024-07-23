ac5b38b46bed892d553fad47b865a744ed7fcc9f
Kalichuza <kalichuza@proton.me>
2024-07-23 13:19:59 -0400

    Update moveUserProfileSafe.ps1

--- userMigration/moveUserProfileSafe.ps1
+++ userMigration/moveUserProfileSafe.ps1
@@ -1,3 +1,12 @@
+#1. Upload the script to the target machine.
+ 
+#2. Set up Mapped Drive to your target folder.
+#	net use J: \\SERVER\Pathe\To\NetDrive /user:domain\administrator P4ssWord
+
+#3. Use this command to initiate the transfer:
+
+#	.\moveFolder2NetDrive.ps1 -source "C:\Path\To\Origin" -destination "J:" -username "domain\administrator" -password 'PassWord'
+
 param (
     [string]$source,
     [string]$destination,
