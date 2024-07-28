7091a952a82e906aa2dd477e97cb3e834426c698
Kalichuza <kalichuza@proton.me>
2024-07-28 07:33:32 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,30 @@
+param (
+    [Parameter(Mandatory=$false)]
+    [switch]$Flag1 = $false,
+
+    [Parameter(Mandatory=$false)]
+    [switch]$Flag2 = $false,
+
+    [Parameter(Mandatory=$false)]
+    [switch]$Flag3 = $false
+)
+
+if ($Flag1) {
+    Write-Output "Flag1 is set."
+} else {
+    Write-Output "Flag1 is not set."
+}
+
+if ($Flag2) {
+    Write-Output "Flag2 is set."
+} else {
+    Write-Output "Flag2 is not set."
+}
+
+if ($Flag3) {
+    Write-Output "Flag3 is set."
+} else {
+    Write-Output "Flag3 is not set."
+}
+
 Write-Host "You have successfully ran this code remotely"
