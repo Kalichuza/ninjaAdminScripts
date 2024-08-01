3880f929e22871de985fcb24be1e9e1c534d945d
Kalichuza <kalichuza@proton.me>
2024-08-01 14:13:39 -0400

    Update pgpEncrypt.ps1

--- Encryption/pgpEncrypt.ps1
+++ Encryption/pgpEncrypt.ps1
@@ -11,36 +11,41 @@
 #>
 
 # Checks to make sure the pspgp module is installed
-function Install-ModulePSPGP {
+[CmdletBinding()]
+param(
+        [Parameter(Mandatory)]
+        [string]$File2Encrypt_Input,
+
+        [Parameter(Mandatory)]
+        [string]$File2Encrypt_Output
+    ) 
+function Encrypt-PgpItem {
     $pspgpInstalled = Get-Module | Where-Object -Property 'Name' -Contains 'pspgp'
+
+    
 
     if ($null -eq $pspgpInstalled) {
         try {
-            Install-Module -Name PSPGP -Force -Scope CurrentUser
             Import-Module PSPGP
             Write-Host "PSPGP Module was installed"
-        } catch {
+        }
+        catch {
             Write-Host "Failed to install PSPGP Module. Please check your internet connection or repository settings."
         }
-    } else {
+    }
+    else {
         Import-Module PSPGP
         Write-Host "PSPGP Module was already installed"
     }
-}
 
+    # Path to your public key
+    $publicKey = "<Path\to\public.key>"
 
-function Encrypt-PGP {
-    param(
-    [string]$File2Encrypt_Input,
-    [string]$File2Encrypt_Output
-)
+    # Encrypts the file
+    
+    Protect-PGP -FilePathPublic $publicKey -FilePath $File2Encrypt_Input -OutFilePath $File2Encrypt_Output
 
-
-$publicKey = "<Path\to\public.key>"
-
-# Encrypts the file
-Protect-PGP -InputFile $File2Encrypt_Input -OutputFile $File2Encrypt_Output -PublicKeyFile $publicKey
-Write-Host "Your file has been encrypted"
+    Write-Host "Your file has been encrypted"
 }
 
 
