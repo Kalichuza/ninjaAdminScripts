53a9bc248ca19522fd8415823e1bd3fd6227156f
Kalichuza <kalichuza@proton.me>
2024-08-12 10:18:40 -0400

    Update New-NinjaWinPeUsb.ps1

--- Windows-ImageDeployment/New-NinjaWinPeUsb.ps1
+++ Windows-ImageDeployment/New-NinjaWinPeUsb.ps1
@@ -10,7 +10,7 @@ New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
  
 New-OSDCloudUSB
  
-Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://gist.githubusercontent.com/Jeffhunter88/ed338a1c3aab4ca6abd2dd68a329d53c/raw/osdcloud_config.ps1 -Verbose
+Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://raw.githubusercontent.com/Kalichuza/ninjaAdminScripts/main/Windows-ImageDeployment/Dynamic-Deploy.ps1 -Verbose
  
 New-OSDCloudISO
  
