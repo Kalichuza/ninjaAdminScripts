ff3805b30fd151df9af2e196b6cc8f8f86b39500
Kalichuza <kalichuza@proton.me>
2024-08-12 11:03:45 -0400

    update

--- Windows-ImageDeployment/Dynamic-Deploy.ps1
+++ Windows-ImageDeployment/Dynamic-Deploy.ps1
@@ -1,20 +1,12 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-# Prompt user for OS selection
-$osChoice = Read-Host "Enter the OS Version to install (Windows 10/Windows 11):"
+Start-OSDCloud -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
 
-# Validate the user's choice and start the deployment
-if ($osChoice -eq '10') {
-    Start-OSDCloud -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
-} elseif ($osChoice -eq '11') {
-    Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
-} else {
-    Write-Host -ForegroundColor Red "Invalid selection. Please run the script again."
-    Exit
-}
+#Restart from WinPE
 
-# Restart from WinPE after installation
-Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
+Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+
 Start-Sleep -Seconds 20
+
 wpeutil reboot
