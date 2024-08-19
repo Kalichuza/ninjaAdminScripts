0ba6a92f8e9c12db288aaa0c2fb26e57af798c51
Kalichuza <kalichuza@proton.me>
2024-08-19 08:49:19 -0400

    Update New-ProxmoxISO.ps1

--- Windows-ImageDeployment/New-ProxmoxISO.ps1
+++ Windows-ImageDeployment/New-ProxmoxISO.ps1
@@ -33,14 +33,12 @@ Edit-OSDCloudWinPE -WorkspacePath C:\OSDCloud -CloudDriver * -AddDriverPath $vir
 # Dismount the VirtIO ISO after use
 Dismount-DiskImage -ImagePath $virtioIsoPath
 
-# Add unattend.xml file or other custom files to the WinPE environment
-Edit-OSDCloudWinPE -WorkspacePath C:\OSDCloud -CloudDriver * -AddFile -FilePath "P:\Path\to\unattend.xml" -Destination "X:\unattend.xml" -Verbose
-
-# Add additional custom files, such as provisioning scripts or other configurations
-Edit-OSDCloudWinPE -WorkspacePath C:\OSDCloud -CloudDriver * -AddFile -FilePath "P:\Path\to\custom\files\*" -Destination "X:\OSDCloud\Automate\Provisioning" -Verbose
-
-# Optionally specify the custom deployment script URL
-Edit-OSDCloudWinPE -WorkspacePath C:\OSDCloud -CloudDriver * -WebPSScript "https://raw.githubusercontent.com/Kalichuza/ninjaAdminScripts/main/Windows-ImageDeployment/Dynamic-Deploy-10.ps1" -Verbose
+# Check if the files are pre-populated correctly
+if (Test-Path "C:\OSDCloud\WinPE\unattend.xml" -and Test-Path "C:\OSDCloud\WinPE\OSDCloud\Automate\Provisioning\*") {
+    Write-Host "Custom files are in place."
+} else {
+    Write-Host "Custom files are missing, please verify paths."
+}
 
 # Create the ISO directly using OSDCloud's built-in capabilities
 New-OSDCloudISO -WorkspacePath C:\OSDCloud
