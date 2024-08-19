7b1f7260094225925ffb2e3d12e2d3005ded57cc
Kalichuza <kalichuza@proton.me>
2024-08-18 20:49:52 -0400

    Update New-ProxmoxISO.ps1

--- Windows-ImageDeployment/New-ProxmoxISO.ps1
+++ Windows-ImageDeployment/New-ProxmoxISO.ps1
@@ -13,6 +13,26 @@ New-OSDCloudTemplate
 # Create OSDCloud workspace
 New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
 
+# Download VirtIO drivers using the direct download link
+$virtioDriversUrl = "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.262-2/virtio-win.iso"
+$virtioIsoPath = "C:\OSDCloud\virtio-win.iso"
+Invoke-WebRequest -Uri $virtioDriversUrl -OutFile $virtioIsoPath
+
+# Mount the VirtIO ISO
+Mount-DiskImage -ImagePath $virtioIsoPath
+
+# Get the drive letter assigned to the mounted VirtIO ISO
+$virtioDriveLetter = (Get-DiskImage -ImagePath $virtioIsoPath | Get-Volume).DriveLetter
+
+# Correctly format the path to the VirtIO drivers
+$virtioDriversPath = $virtioDriveLetter + ":\"
+
+# Add VirtIO drivers to the Windows PE environment
+Edit-OSDCloudWinPE -WorkspacePath C:\OSDCloud -CloudDriver * -AddDriverPath $virtioDriversPath -Verbose
+
+# Dismount the VirtIO ISO after use
+Dismount-DiskImage -ImagePath $virtioIsoPath
+
 # Edit the Windows PE environment and add your custom files. For example, msi files, ppkg files, or unattend.xml files
 Edit-OSDCloudWinPE -WorkspacePath C:\OSDCloud -CloudDriver * -AddFile -FilePath "P:\Path\to\unattend.xml" -Destination "X:\unattend.xml" -Verbose
 
