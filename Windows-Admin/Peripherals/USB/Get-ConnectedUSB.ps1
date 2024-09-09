7157b91cb528bafe30a5fa4aef127b3d81a9c4c3
Kalichuza <kalichuza@proton.me>
2024-09-09 11:44:49 -0400

    refactor: Improve query in Get-ConnectedUSB.ps1 script

--- Windows-Admin/Peripherals/USB/Get-ConnectedUSB.ps1
+++ Windows-Admin/Peripherals/USB/Get-ConnectedUSB.ps1
@@ -4,7 +4,10 @@ $usbDevices = Get-WmiObject -Query "SELECT * FROM Win32_USBControllerDevice"
 # Output the USB devices
 foreach ($device in $usbDevices) {
     $devicePath = [System.Management.ManagementObject]($device.Dependent)
-    $deviceInfo = Get-WmiObject -Query "SELECT * FROM Win32_PnPEntity WHERE DeviceID='$($devicePath.DeviceID)'"
+    # Escape backslashes in DeviceID for WMI query
+    $escapedDeviceID = $devicePath.DeviceID -replace '\\', '\\\\'
+    $deviceInfo = Get-WmiObject -Query "SELECT * FROM Win32_PnPEntity WHERE DeviceID='$escapedDeviceID'"
+    
     if ($deviceInfo) {
         $deviceInfo | Select-Object Name, DeviceID, Manufacturer, Description
     }
